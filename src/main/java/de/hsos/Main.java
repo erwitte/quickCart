a4678aa55c389732499d14a2706b78b499b5d526
Erik Witte <witte.erik2@gmail.com>
2025-02-05 14:19:39 +0100

    keycloak runs on start up

--- src/main/java/de/hsos/Main.java
+++ src/main/java/de/hsos/Main.java
@@ -3,11 +3,29 @@ package de.hsos;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import io.quarkus.runtime.Quarkus;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @QuarkusMain
 public class Main {
 
     public static void main(String ... args) {
-        System.out.println("Running main method");
-        Quarkus.run(args);
+        try{
+            ProcessBuilder processBuilder = new ProcessBuilder("docker-compose", "-f", "src/main/resources/keycloak/keycloak.yaml", "up");
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+            Quarkus.run(args);
+            
+            // Read and print the output
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
